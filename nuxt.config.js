020e880df039e23f142f60f1a9bf06b4d57e5e86
elirehema <rocksandystone@gmail.com>
2022-09-09 14:27:12 +0300

    Update display property

--- nuxt.config.js
+++ nuxt.config.js
@@ -119,6 +119,7 @@ export default {
       color_theme: "#A0D18C",
       background_color: "#A0D18C",
       lang: 'en',
+      display: 'browser',
       useWebmanifestExtension: false
     },
     meta: {
