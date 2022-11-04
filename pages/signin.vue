4a27234c18f9aba12d498b45e435a0208259d027
elirehema <rocksandystone@gmail.com>
2022-11-04 15:42:37 +0300

    Added reset button

--- pages/signin.vue
+++ pages/signin.vue
@@ -5,71 +5,42 @@
         <v-card class="elevation-12" width="400">
           <v-toolbar prominent :src="image" flat>
             <v-toolbar-title color="white" dense>
-              <img
-                height="100px"
-                width="100"
-                class="mt-6"
-                src="../assets/images/logo.png"
-                alt=""
-              />
+              <img height="100px" width="100" class="mt-6" src="../assets/images/logo.png" alt="" />
             </v-toolbar-title>
             <v-spacer></v-spacer>
           </v-toolbar>
           <v-card-text>
-            <v-form
-              @submit.prevent="login"
-              id="check-login-form"
-              class="mt-3"
-              v-model="valid"
-            >
+            <v-form @submit.prevent="login" id="check-login-form" class="mt-3" v-model="valid">
               <v-container>
                 <v-row>
                   <v-col cols="12" sm="12">
-                    <v-text-field
-                      rounded
-                      label="Username"
-                      placeholder="e.g. demo"
-                      v-model="payload.username"
-                      prepend-inner-icon="person"
-                      :rules="[rules.required]"
-                      value="user"
-                      required
-                      outlined
-                    ></v-text-field>
+                    <v-text-field rounded label="Username" placeholder="e.g. demo" v-model="payload.username"
+                      prepend-inner-icon="person" :rules="[rules.required]" value="user" required outlined>
+                    </v-text-field>
 
-                    <v-text-field
-                      rounded
-                      v-model="payload.password"
-                      prepend-inner-icon="lock"
-                      :append-icon="show1 ? 'visibility' : 'visibility_off'"
-                      :rules="[rules.required, rules.min]"
-                      :type="show1 ? 'text' : 'password'"
-                      name="input-10-1"
-                      label="Password"
-                      placeholder="e.g ********"
-                      hint="At least 8 characters"
-                      counter
-                      @click:append="show1 = !show1"
-                      required
-                      outlined
-                    ></v-text-field>
+                    <v-text-field rounded v-model="payload.password" prepend-inner-icon="lock"
+                      :append-icon="show1 ? 'visibility' : 'visibility_off'" :rules="[rules.required, rules.min]"
+                      :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" placeholder="e.g ********"
+                      hint="At least 8 characters" counter @click:append="show1 = !show1" required outlined>
+                    </v-text-field>
                   </v-col>
                 </v-row>
               </v-container>
             </v-form>
           </v-card-text>
           <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              block
-              large
-              rounded
-              color="primary"
-              type="submit"
-              :disabled="!valid"
-              form="check-login-form"
-              >Login</v-btn
-            >
+            <v-row no-gutters>
+              <v-col cols="12">
+                <v-btn block large rounded color="primary" type="submit" :disabled="!valid" form="check-login-form">
+                  Login
+                </v-btn>
+              </v-col>
+              <v-col cols="12" class="d-flex justify-center py-4">
+                <v-btn block text rounded small class="blue--text text-normal"
+                  @click="$store.dispatch('_updatetenant', 'demo')">
+                  Click Here to reset</v-btn>
+              </v-col>
+            </v-row>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -92,11 +63,11 @@ export default {
     show2: true,
     show3: false,
     show4: false,
-    payload:{
-       password: "Password1!",
-       username: "selfservice",
+    payload: {
+      password: "Password1!",
+      username: "selfservice",
     },
-    image:"https://vicsguide.com/wp-content/uploads/2021/07/Best-loan-apps.jpg",
+    image: "https://vicsguide.com/wp-content/uploads/2021/07/Best-loan-apps.jpg",
     rules: {
       required: value => !!value || "Required.",
       min: v => v.length >= 8 || "Min 8 characters",
@@ -108,7 +79,7 @@ export default {
   },
   methods: {
     login() {
-      this.$store.dispatch("_authenticate",  this.payload)
+      this.$store.dispatch("_authenticate", this.payload)
     },
     nativateToHere(id) {
       this.$router.push('/' + id);
@@ -127,3 +98,4 @@ export default {
   }
 };
 </script>
+
