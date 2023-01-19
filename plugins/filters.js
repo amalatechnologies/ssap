d278257f77a932282f4162992abedaf755615cf3
elirehema <rocksandystone@gmail.com>
2023-01-19 12:37:38 +0300

    Update date format

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,24 +1,23 @@
-
 import Vue from "vue";
-Vue.filter('percentage', function (value) {
-  if (value == null) return ''
-  value = value.toString()
-  return value + ".00%"
-})
+Vue.filter("percentage", function (value) {
+  if (value == null) return "";
+  value = value.toString();
+  return value + ".00%";
+});
 
-Vue.filter('currency', function (value) {
-  if (value == null) return ''
-  value = value.toString()
-  return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-})
+Vue.filter("currency", function (value) {
+  if (value == null) return "";
+  value = value.toString();
+  return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+});
 
-Vue.filter('dateformat', function (value) {
-  if (!value) return ''
-  const date_string = value[1] + "-" + value[2] + "-" + value[0];
+Vue.filter("dateformat", function (value) {
+  if (!value) return "";
+  const date_string = value[2] + "-" + value[1] + "-" + value[0];
   return new Date(date_string).toDateString();
-})
-Vue.filter('simpledateformat', function (value) {
-  if (!value) return ''
+});
+Vue.filter("simpledateformat", function (value) {
+  if (!value) return "";
   const date_string = value[2] + "-" + value[1] + "-" + value[0];
   return date_string;
-})
+});
