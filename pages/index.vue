7cf27866e972a6aaaa7dcde97c55268a81fa5d30
elirehema <rocksandystone@gmail.com>
2024-12-16 00:30:57 +0300

    Add download dialog button

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,77 @@
 <template>
   <v-container class="ma-0 pa-0" fluid>
-    <v-card elevation="0" color="primary darken-1" class="mx-auto" min-height="120" tile>
-      <v-list-item v-if="profile" three-line class="pt-5" color="rgba(0, 0, 0, .4)" dark>
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
+          Click Me
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="text-h5 grey primary"> Updates </v-card-title>
+
+        <v-card-text>
+          For Android users you can no download new version of Amala Yangu
+          directly from Play Store and enjoy improved look and feel. For iOS
+          users we will update you soon. Click Download or Cancel otherwise
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="warning" small @click="dialog = false"> Cancel </v-btn>
+          <v-btn color="green" @click="dialog = false" small>
+            <a
+              class="white--text"
+              href="https://play.google.com/store/apps/details?id=org.mifos.amalayangu"
+              target="_blank"
+            >
+              <v-icon left>mdi-cloud-download</v-icon>
+              Download
+            </a>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-card
+      elevation="0"
+      color="primary darken-1"
+      class="mx-auto"
+      min-height="120"
+      tile
+    >
+      <v-list-item
+        v-if="profile"
+        three-line
+        class="pt-5"
+        color="rgba(0, 0, 0, .4)"
+        dark
+      >
         <v-list-item-icon class="pa-0">
           <nuxt-link to="/me">
             <v-avatar color="primary" size="62">
               <span v-if="profile.displayName" class="white--text text-h4">{{
-                  profile.displayName.charAt(0)
+                profile.displayName.charAt(0)
               }}</span>
             </v-avatar>
           </nuxt-link>
         </v-list-item-icon>
         <v-list-item-content>
-          <v-list-item-subtitle class="white--text">Hello, {{ profile.displayName }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="white--text"
+            >Hello, {{ profile.displayName }}</v-list-item-subtitle
+          >
         </v-list-item-content>
       </v-list-item>
     </v-card>
     <v-row no-gutters class="pt-3 px-3">
       <v-col class="d-flex justify-space-between align-center">
         <p class="pt-1">Account overview</p>
-        <v-btn v-if="accountvisible" @click="accountvisible = !accountvisible" icon>
+        <v-btn
+          v-if="accountvisible"
+          @click="accountvisible = !accountvisible"
+          icon
+        >
           <v-icon>mdi-eye</v-icon>
         </v-btn>
         <v-btn @click="accountvisible = !accountvisible" icon v-else>
@@ -30,17 +82,10 @@
     <v-row class="py-2" v-if="accountvisible">
       <v-col cols="5" sm="5" class="d-flex justify-center">
         <div class="pt-2">
-          <p class="
-              d-flex
-              red--text
-              justify-center
-              align-bottom
-              pa-0
-              ma-0
-              mt-2
-              text-h6
-              font-weight-bold
-            " v-if="savingbalance">
+          <p
+            class="d-flex red--text justify-center align-bottom pa-0 ma-0 mt-2 text-h6 font-weight-bold"
+            v-if="savingbalance"
+          >
             {{ savingbalance | currency }}
           </p>
           <p class="d-flex pa-0 ma-0 justify-center align-top">Total Savings</p>
@@ -51,17 +96,10 @@
       </v-col>
       <v-col cols="5" sm="5" class="d-flex justify-start">
         <div class="pt-2">
-          <p class="
-              d-flex
-              red--text
-              justify-center
-              align-bottom
-              pa-0
-              ma-0
-              mt-2
-              text-h6
-              font-weight-bold
-            " v-if="loanbalance">
+          <p
+            class="d-flex red--text justify-center align-bottom pa-0 ma-0 mt-2 text-h6 font-weight-bold"
+            v-if="loanbalance"
+          >
             {{ loanbalance | currency }}
           </p>
           <p class="d-flex pa-0 ma-0 justify-center align-top">Total Loans</p>
@@ -70,11 +108,20 @@
     </v-row>
     <v-card elevation="0" color="primary darken-1" class="py-5 mt-3" tile>
       <v-row no-gutters>
-        <v-col class="d-flex justify-center mb-2 white--text" cols="12">Manage Accounts</v-col>
+        <v-col class="d-flex justify-center mb-2 white--text" cols="12"
+          >Manage Accounts</v-col
+        >
         <v-col cols="4" class="ma-0 pa-0">
           <v-row no-gutters class="d-flex justify-center">
             <v-col class="d-flex flex-column align-center">
-              <v-btn class="mb-0 mt-4" to="/accounts" x-large fab elevation="1" color="primary ">
+              <v-btn
+                class="mb-0 mt-4"
+                to="/accounts"
+                x-large
+                fab
+                elevation="1"
+                color="primary "
+              >
                 <v-icon dark> mdi-bank </v-icon>
               </v-btn>
               <p class="mt-2 white--text">Accounts</p>
@@ -85,7 +132,14 @@
         <v-col cols="4" class="ma-0 pa-0">
           <v-row no-gutters class="d-flex justify-center">
             <v-col class="d-flex flex-column align-center">
-              <v-btn class="mb-0 mt-4" to="/accounts" x-large fab elevation="1" color="primary ">
+              <v-btn
+                class="mb-0 mt-4"
+                to="/accounts"
+                x-large
+                fab
+                elevation="1"
+                color="primary "
+              >
                 <v-icon dark> mdi-swap-horizontal </v-icon>
               </v-btn>
               <p class="mt-2 white--text">Transfer</p>
@@ -96,7 +150,14 @@
         <v-col cols="4" class="ma-0 pa-0">
           <v-row no-gutters class="d-flex justify-center">
             <v-col class="d-flex flex-column align-center">
-              <v-btn class="mb-0 mt-4" to="/accounts/apply" x-large fab elevation="1" color="primary ">
+              <v-btn
+                class="mb-0 mt-4"
+                to="/accounts/apply"
+                x-large
+                fab
+                elevation="1"
+                color="primary "
+              >
                 <v-icon dark> mdi-draw-pen </v-icon>
               </v-btn>
               <p class="mt-2 white--text">Apply for Loan</p>
@@ -112,7 +173,14 @@
         <v-col cols="4" class="ma-0 pa-0">
           <v-row no-gutters class="d-flex justify-center">
             <v-col class="d-flex flex-column align-center">
-              <v-btn class="mb-0 mt-4" to="/beneficiaries" x-large fab elevation="1" color="white">
+              <v-btn
+                class="mb-0 mt-4"
+                to="/beneficiaries"
+                x-large
+                fab
+                elevation="1"
+                color="white"
+              >
                 <v-icon dark> mdi-account-supervisor </v-icon>
               </v-btn>
               <p class="mt-2">Beneficiaries</p>
@@ -123,7 +191,14 @@
         <v-col cols="4" class="ma-0 pa-0">
           <v-row no-gutters class="d-flex justify-center">
             <v-col class="d-flex flex-column align-center">
-              <v-btn class="mb-0 mt-4" to="/charges" x-large fab elevation="1" color="white">
+              <v-btn
+                class="mb-0 mt-4"
+                to="/charges"
+                x-large
+                fab
+                elevation="1"
+                color="white"
+              >
                 <v-icon dark> mdi-wallet </v-icon>
               </v-btn>
               <p class="mt-2">Charges</p>
@@ -153,10 +228,12 @@ export default {
   data() {
     return {
       accountvisible: true,
+      dialog: false,
     };
   },
   methods: {},
   created() {
+    this.dialog = true;
     this.$store.dispatch("selfserviceclient");
     this.$store.dispatch("_getaccounts", this.clientId);
     this.$store.dispatch("_retrieveguarantors", this.clientId);
