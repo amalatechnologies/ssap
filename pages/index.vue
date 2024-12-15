aaa2a3eca1d312c9d58cfc388e4a4313f7fe8f7b
elirehema <rocksandystone@gmail.com>
2024-12-16 01:04:01 +0300

    Remove activator button

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <template>
   <v-container class="ma-0 pa-0" fluid>
     <v-dialog v-model="dialog" width="500">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
-          Click Me
-        </v-btn>
-      </template>
-
       <v-card>
         <v-card-title class="text-h5 grey primary"> Updates </v-card-title>
 
